0e2a7cb06d98e8f4d14156f0d7d08d1acb049de0
Markus <markushedin97@gmail.com>
2023-11-13 05:28:04 +0100

    kavelgrupp

--- baked-feta-pasta.php
+++ baked-feta-pasta.php
@@ -6,9 +6,10 @@
             <!--Content will be here :)-->
         </div>
         <figure class="illustration-container">
-            <img src="../img/kavelgrupp.webp" class="img-pa2" alt="Dekorativ illustration i form av en kökskavel">
+            <img src="img/kavelgrupp.webp" class="img-pa2" alt="Dekorativ illustration i form av en kökskavel">
         </figure>
     </main>
+   
     <?php
         include("inc/footer.php");
     ?>
