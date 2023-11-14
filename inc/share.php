e908f150ede435fcedc7fe4c877d7848f13e3f4e
Markus <markushedin97@gmail.com>
2023-11-14 02:04:22 +0100

    share

---
+++ inc/share.php
@@ -0,0 +1,19 @@
+<div class="share-container">
+        <button class="share-btn">
+            <i class="fas fa-share-alt"></i>
+        </button>
+        <div class="share-options">
+            <div class="social-media">
+                <button class="social-media-btn" onclick="copyToClipboard('https://kreativkost.se/')">
+                    <i class="far fa-copy"></i>
+                  </button>
+                <button onclick="window.location.href='https://www.instagram.com/';" class="social-media-btn">
+                    <i class="fab fa-instagram"></i>
+                  </button>                  
+                <button onclick="window.location.href='https://www.facebook.com/';" class="social-media-btn">
+                    <i class="fab fa-facebook-f"></i>
+                  </button>                  
+               
+            </div>
+        </div>
+    </div>
