47252913ef3597aeb383da8b844265d9af0ddcd3
Markus <markushedin97@gmail.com>
2023-11-24 15:17:09 +0100

    pad thai

---
+++ padthai.php
@@ -0,0 +1,19 @@
+<?php include("inc/header.php");?>
+
+    <main class="container no-padding">
+    <?php include("inc/share.php");?>
+        <img src="../img/kavel.png" class="img-pa" alt="Dekorativ illustration i form av en kökskavel">
+        <div class="recept" id="3">
+            <!--Content will be here :)-->
+        </div>
+        <figure class="illustration-container">
+            <img src="img/kavelgrupp.webp" class="img-pa2" alt="Dekorativ illustration i form av en kökskavel">
+        </figure>
+    </main>
+    <?php
+        include("inc/footer.php");
+    ?>
+    <script src="../../js/script.js"></script>
+    <script src="../../js/getRecept.js" defer></script>
+</body>
+</html>
